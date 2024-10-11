9601c62a35f714ec1fa9934f792f107000a8d3c1
Tomek62 <thomaslfb3@gmail.com>
2024-10-11 11:06:28 +0200

    PROGRES blurbackground optimization

--- components/ui/blur-background.tsx
+++ components/ui/blur-background.tsx
@@ -1,10 +1,12 @@
 const BlurBackground = () => {
-    return(
+    return (
         <div>
-            <div className="absolute -left-1/4 top-16 z-[-1] h-1/2 w-full scale-150 rounded-full bg-gradient-to-r from-primary to-white opacity-60 blur-3xl"></div>
-          <div className="absolute -right-1/4 bottom-0 z-[-1] h-1/2 w-full scale-150 rounded-full bg-gradient-to-l from-primary to-white opacity-60 blur-3xl"></div>
-          <div className="absolute right-1 top-2.5 z-[-1] h-1/3 w-full scale-150 rounded-full bg-gradient-to-bl from-bgHeroPink to-white opacity-40 blur-3xl"></div>
-          <div className="absolute right-1/3 top-2/3 z-[-1] h-1/3 w-1/3  scale-150 rounded-full bg-white opacity-70 blur-3xl"></div>
+            {/* Optimisation des tailles et du flou */}
+            <div className="absolute -left-1/4 top-16 z-[-1] h-1/3 w-full scale-125 rounded-full bg-gradient-to-r from-primary to-white opacity-50 blur-xl will-change-transform will-change-opacity"></div>
+            <div className="absolute -right-1/4 bottom-0 z-[-1] h-1/3 w-full scale-125 rounded-full bg-gradient-to-l from-primary to-white opacity-50 blur-xl will-change-transform will-change-opacity"></div>
+            <div className="absolute right-1 top-2.5 z-[-1] h-1/4 w-full scale-125 rounded-full bg-gradient-to-bl from-bgHeroPink to-white opacity-30 blur-lg will-change-transform will-change-opacity"></div>
         </div>
-    )}
+    );
+}
+
 export default BlurBackground;
