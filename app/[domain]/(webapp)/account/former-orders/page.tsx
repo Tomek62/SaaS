758a50159c483414b3f89dfb0a3ed5fe0349ddc9
Thomas Lefebvre <thomaslfb3@gmail.com>
2025-01-20 09:31:54 +0100

    DELETE former element and pages

--- app/[domain]/(webapp)/account/former-orders/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function FormerOrders() { 
-    return (
-        <div>
-            FormerOrders
-        </div>
-    );  
-}  
--- app/[domain]/(webapp)/fidelity/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Fidelity() {
-  return (
-    <div className="flex flex-col space-y-6">
-      <h1>carte de fidélité</h1>
-    </div>
-  );
-}
